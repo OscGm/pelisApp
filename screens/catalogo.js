5bc858caa68a25e26b9d9f493f1ce1ef6d3d152d
Oscar Aviña Gama <a17310024@ceti.mx>
2021-01-14 21:25:31 -0600

    se muestran las imagenes en toda la pantalla falta organizarla

--- screens/catalogo.js
+++ screens/catalogo.js
@@ -4,12 +4,12 @@ import { StyleSheet, View, Text, Image, ScrollView, SafeAreaView } from 'react-n
 
 const catalogo = ({navigation}) => {
 
-  const URL_API = 'https://api.themoviedb.org/3/movie/popular?api_key=02f70a2fb7623e38786e1316db596f10'
+  const URL_API_POPULARES = 'https://api.themoviedb.org/3/movie/popular?api_key=02f70a2fb7623e38786e1316db596f10'
   const IMAGES_API = 'https://image.tmdb.org/t/p/w185'
   const [movies, setMovies] = useState([]);
   
   useEffect (()=>{
-    fetch(URL_API)
+    fetch(URL_API_POPULARES)
     .then((res) => res.json())
     .then((data) => {
       // Ver datos de la api
@@ -18,16 +18,18 @@ const catalogo = ({navigation}) => {
     });
   }, []);
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <ScrollView>
+      <>
+      <ScrollView>
+        <Text style={{alignContent:'center'}}>
           {movies.length > 0 && movies.map((movie) => <Image key={movie.id}
-            style={{width:50, height:50}}
+            style={{width:100, height:100}}
             source={{
               uri:IMAGES_API+movie.poster_path
             }}
           />)}
+        </Text>
         </ScrollView>
-      </SafeAreaView>
+      </>
     )
 }
 
